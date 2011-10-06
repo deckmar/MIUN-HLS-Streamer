2ced635c51594b759edbd2564fc50108f5ce3316
alexandre <alexandre@10.250.11.173>
2011-10-06 15:49:20 +0200

    adding menus (version 1)

--- src/se/miun/hls/MainActivity.java
+++ src/se/miun/hls/MainActivity.java
@@ -9,13 +9,19 @@ import java.net.URLConnection;
 import java.util.Vector;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.webkit.URLUtil;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class MainActivity extends Activity implements OnCompletionListener {
@@ -72,7 +78,7 @@ public class MainActivity extends Activity implements OnCompletionListener {
 	public void onCompletion(MediaPlayer mp) {
 		playNextVideo();
 	}
-//aeazaza
+
 	private void playNextVideo() {
 		if (this.video_uri_list.size() > ++this.video_uri_iteration_index) {
 
@@ -87,5 +93,44 @@ public class MainActivity extends Activity implements OnCompletionListener {
 		}
 	}
 	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(0, 1, 1, "Quality").setIcon(android.R.drawable.ic_menu_view);
+		menu.add(0, 2, 2, "URL").setIcon(android.R.drawable.ic_menu_search);
+		//menu.add(0, 1, 1, "Settings").setIcon(android.R.drawable.ic_menu_preferences);
+		menu.add(0, 3, 3, "Exit").setIcon(android.R.drawable.ic_menu_close_clear_cancel);
+		return true;
+	}
 	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()){
+		case 1:
+			final String[] qualities = {"Quality1, Quality2, Quality3, Quality4"};
+			AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+			dialog.setTitle("Select a quality");
+			dialog.setItems(qualities, new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface diaInt, int quality) {
+					Toast.makeText(getApplicationContext(), qualities[quality], Toast.LENGTH_SHORT).show();
+				}
+			}).show();
+			break;
+		case 2:
+			final EditText ed = new EditText(this);
+			ed.setText("Open URL");
+			final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setView(ed)
+			.setPositiveButton("Open", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialogInt, int whichButton) {
+					
+				}
+			}).show();
+			break;
+		case 3:
+			break;
+		}
+		return true;
+	}
 }
