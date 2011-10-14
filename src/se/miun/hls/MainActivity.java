47de99d5bbf229c0cb592156724ae89e080942df
alexandre <alexandre@192.168.1.2>
2011-10-14 11:01:37 +0200

    layouts...

--- src/se/miun/hls/MainActivity.java
+++ src/se/miun/hls/MainActivity.java
@@ -201,6 +201,7 @@ public class MainActivity extends Activity implements OnCompletionListener,
 
 	@Override
 	public void onCompletion(MediaPlayer mp) {
+		video.bringToFront();
 		playNextVideo();
 	}
 
